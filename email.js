e58c9d99d8285d137e4cbaf1f15daca1395bad2d
Anthony Kinsey <anthony@slickage.com>
2014-11-13 16:21:34 -1000

    Fix for reset_password parameter. resetPassword->reset_password

--- email.js
+++ email.js
@@ -10,7 +10,7 @@ var config = require(path.join(__dirname, 'config'));
 exports.recoverAccount = {
   schema: {
     username: Joi.string().min(1).max(255).required(),
-    reset_token: Joi.string().token().length(20).required(),
+    reset_token: Joi.string().token().length(40).required(),
     email: Joi.string().email().required()
   },
   compile: function (params) {
@@ -19,7 +19,7 @@ exports.recoverAccount = {
       from: config.senderEmail,
       to: params.email,
       subject: '[EpochTalk] Account Recovery',
-      html: template({ username: params.username, rootUrl: config.hostUrl, resetToken: params.resetToken})
+      html: template({ username: params.username, rootUrl: config.hostUrl, resetToken: params.reset_token})
     };
   }
 };
