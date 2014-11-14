61b46ed8542245f3d6aeaaa6a487d267f26f8b3b
Anthony Kinsey <anthony@slickage.com>
2014-11-13 16:42:28 -1000

    Added support for setting config variables via environment variables

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 'use strict';
 
 var config = {
-  socketPath: '/tmp/epochEmailer.sock',
-  senderEmail: 'info@epochtalk.com',
-  hostUrl: 'http://localhost:8080',
-  logEnabled: true
+  socketPath: process.env.EMAILER_SOCKET_PATH || '/tmp/epochEmailer.sock',
+  senderEmail: process.env.EMAILER_SENDER_EMAIL || 'info@epochtalk.com',
+  hostUrl: process.env.EMAILER_HOST_URL || 'http://localhost:8080',
+  logEnabled: process.env.EMAILER_LOG_ENABLED || true
 };
 
 module.exports = config;
