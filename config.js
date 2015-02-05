2e6bd1dc21df69362dd3237ac77aec167357a41e
James Wang <jw@slickage.com>
2015-02-04 15:44:36 -1000

    Emailer socket path defaults not in /tmp

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
-/* jshint node: true */
-'use strict';
+var emailerSocketPath = '/var/run/epochEmailer.sock';
+if (os === 'darwin') {
+  emailerSocketPath = '/tmp/epochEmailer.sock';
+}
 
 var config = {
-  socketPath: process.env.EMAILER_SOCKET_PATH || '/tmp/epochEmailer.sock',
+  socketPath: process.env.EMAILER_SOCKET_PATH || emailerSocketPath,
   senderEmail: process.env.EMAILER_SENDER_EMAIL || 'info@epochtalk.com',
   logEnabled: process.env.EMAILER_LOG_ENABLED || true
 };
